785dd10907edff49d211a176b73e6875c228b9cc
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-10-08 19:02:02 +0800

    Update MenuActivity.kt

--- MyApplication2/app/src/main/java/com/example/myapplication/MenuActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -66,7 +66,7 @@ class MenuActivity : AppCompatActivity() {
             editor.apply()
 
 
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, HomeActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
