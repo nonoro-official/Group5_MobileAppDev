f5f6ad6e28a089286527014b3b834bc22977ca3b
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-10-08 18:13:36 +0800

    home done

--- MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,11 +1,42 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val act1Btn = findViewById<Button>(R.id.act1_btn)
+        val act2Btn = findViewById<Button>(R.id.act2_btn)
+        val act3Btn = findViewById<Button>(R.id.act3_btn)
+
+        act1Btn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        act2Btn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        act3Btn.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
