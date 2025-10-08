dd2dbf9a8a017204b0aef42cede17cb12095b03e
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-10-08 17:22:31 +0800

    Update HomeActivity.kt

--- MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatDelegate
 class HomeActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
 
