129972c12dd63543cd2e1044af98143c54b10397
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-10-11 11:44:25 +0800

    fixed system status color for home

--- MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import kotlin.math.log
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBars.top, 0, 0) // push content below status bar
+            insets
+        }
+
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.isAppearanceLightStatusBars = true // white icons
+
 
         val loginBtn = findViewById<ImageButton>(R.id.login_btn)
 
